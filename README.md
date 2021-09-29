3f75a2cc2accdc885ed4bd791dfdaf5e2cdcdb87
unknown <nobel10122025@gmail.com>
2021-09-29 12:25:42 +0530

    added api and styling

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Quote Generator designed with Javascript
 
 
 It is a very simple but Vibrant code generator designed with the help of 
-1. html
+1. Html
 2. CSS
 3. Javascript
-
+4. Ajax
--- scripts.js
+++ scripts.js
@@ -1,37 +1,35 @@
-quotes=[
-    '"Life is what happens when you\'re busy making other plans." - John Lennon',
-    '"Get busy living or get busy dying." - Stephen King.',
-    '"You only live once, but if you do it right, once is enough." - Mae West.',
-    '"Many of life’s failures are people who did not realize how close they were to success when they gave up." - Thomas A. Edison',
-    '"If you want to live a happy life, tie it to a goal, not to people or things." - Albert Einstein',
-    '"Never let the fear of striking out keep you from playing the game." - Babe Ruth',
-    '"Money and success don’t change people; they merely amplify what is already there." - Will Smith',
-    '"Not how long, but how well you have lived is the main thing." - Seneca',
-    '"The whole secret of a successful life is to find out what is one’s destiny to do, and then do it." - Henry Ford',
-    '"The big lesson in life, baby, is never be scared of anyone or anything." - Frank Sinatra',
-    '"Life is not a problem to be solved, but a reality to be experienced." - Soren Kierkegaard'
-]
+const btn = document.getElementById('button')
+const quote =  document.getElementById('quote')
+const author = document.getElementById('author')
+const links = document.getElementById('link')
+const Select_body =document.getElementsByTagName('body')[0]
 
-colors=['#FF00BF','#FF00BF','green','violet','tomato','#F9629F','#1d1160','#662d91','#452c63','#720e9e','#AA0000','#800020','teal']
-function Quotes()
-{   let quotenumber=Math.floor(Math.random()*quotes.length)
-    //console.log(quotes[quotenumber])
-    let colornumber = Math.floor(Math.random()*colors.length)
-    //document.getElementById('quote').innerText=quotes[quotenumber]
-    for(var i=0; i<quotes[quotenumber].length;i++)
-    {   //console.log(quotes[quotenumber][i])
-        if(quotes[quotenumber][i]=='-')
-        {
-            break
-        }
-    }
-    // console.log(i)
-    // console.log(quotes[quotenumber].slice(i+1,quotes[quotenumber].length))
-    document.getElementById('quote').innerText=quotes[quotenumber].slice(0,i-1)
-    document.getElementById('author').innerText=quotes[quotenumber].slice(i+1,quotes[quotenumber].length)
-    document.getElementById('quote').style.color = colors[colornumber]
-    document.getElementById('author').style.color= colors[colornumber]
-    document.body.style.backgroundColor = colors[colornumber]; 
-    document.getElementById('button').style.background = colors[colornumber]
-    document.getElementById('link').style.color = colors[colornumber]
+// getting data from api 
+const generateQuote = async() =>{
+    const response = await fetch('https://api.quotable.io/random')
+    const data = await response.json()
+    quote.innerText=data.content
+    author.innerText = data.author
 }
+btn.addEventListener('click',generateQuote)
+generateQuote()
+
+// playing with colors
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+btn.addEventListener('click',()=>{
+    let random_color = getRandomColor()
+
+    //add or remove any element accordingly 
+    const arr_color_change = [quote ,author ,links]
+    arr_color_change.map((element)=>element.style.color = random_color)
+    const arr_bg_change = [Select_body,btn]
+    arr_bg_change.map((element)=>element.style.backgroundColor=random_color)
+
+})
